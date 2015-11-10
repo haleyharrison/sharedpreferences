ffe3e06397097118b3fe93aedbfab26d09946242
haleyharrison <harrison.haley@gmail.com>
2015-11-10 14:41:00 -0800

    initial file add, username saved.

---
+++ app/src/main/java/com/epicodus/sharedpreferencesdemo/MainActivity.java
@@ -0,0 +1,50 @@
+package com.epicodus.sharedpreferencesdemo;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.epicodus.sharedpreferencesdemo", Context.MODE_PRIVATE);
+
+        sharedPreferences.edit().putString("username", "haley").apply();
+
+        String username = sharedPreferences.getString("username", "");
+
+        Log.i("username", username);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
